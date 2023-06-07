153d711b9ee9f4b36d7177124ed6049e404b0fe0
Sourav Majumder <souravmajumderk99@gmail.com>
2023-06-07 20:46:28 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
+import Analytics from "./components/Analytics";
+import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Newsletter from "./components/Newsletter";
 
 function App() {
   return (
@@ -7,6 +11,10 @@ function App() {
       <div>
         <Navbar />
         <Hero />
+        <Analytics />
+        <Newsletter />
+        <Cards />
+        <Footer />
       </div>
     </>
   );
